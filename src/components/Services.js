cadb9099381b95ab7d8c53f9be94626ea293d768
Jason Cheong <jasonmcheong@gmail.com>
2019-03-16 20:03:47 -0600

    finished styling the services section

--- src/components/Services.js
+++ src/components/Services.js
@@ -10,7 +10,13 @@ class Services extends React.Component {
     }
 
     handleClick(evt) {
-        console.log(evt.source_url);
+        document.getElementById('service-display').innerHTML = `
+            <div>
+                <h3>${evt.title}</h3>
+                <p>${evt.desc}</p>
+            </div>
+            ${evt.video}
+        `;
     }
 
     componentWillMount() {
@@ -31,7 +37,71 @@ class Services extends React.Component {
                                         <div dangerouslySetInnerHTML={{ __html: services.acf.heading_description }} />
                                     </div>
                                 </div>
-                                <div className='services-infographic' />
+                                <div className='service-tabs'>
+                                    <div
+                                        onClick={evt =>
+                                            this.handleClick(
+                                                { title: services.acf.service_1_title, desc: services.acf.service_1_description, video: services.acf.service_1_video_link },
+                                                evt
+                                            )
+                                        }
+                                    >
+                                        <img src={services.acf.service_1_icon.url} alt='icon' width='25px' height='25px' />
+                                        <h2>{services.acf.service_1_title}</h2>
+                                    </div>
+                                    <div
+                                        onClick={evt =>
+                                            this.handleClick(
+                                                { title: services.acf.service_2_title, desc: services.acf.service_2_description, video: services.acf.service_2_video_link },
+                                                evt
+                                            )
+                                        }
+                                    >
+                                        <img src={services.acf.service_2_icon.url} alt='icon' width='25px' height='25px' />
+                                        <h2>{services.acf.service_2_title}</h2>
+                                    </div>
+                                    <div
+                                        onClick={evt =>
+                                            this.handleClick(
+                                                { title: services.acf.service_3_title, desc: services.acf.service_3_description, video: services.acf.service_3_video_link },
+                                                evt
+                                            )
+                                        }
+                                    >
+                                        <img src={services.acf.service_3_icon.url} alt='icon' width='25px' height='25px' />
+                                        <h2>{services.acf.service_3_title}</h2>
+                                    </div>
+                                </div>
+                                <div id='service-display'>
+                                    <div>
+                                        <h3>{services.acf.service_1_title}</h3>
+                                        <p>{services.acf.service_1_description}</p>
+                                    </div>
+                                    <div dangerouslySetInnerHTML={{ __html: services.acf.service_1_video_link }} />
+                                </div>
+                                <div
+                                    className='services-infographic'
+                                    style={{
+                                        width: '100%',
+                                        height: '578px',
+                                        backgroundImage: `url(${services.acf.infographic.url})`,
+                                        backgroundSize: 'cover',
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundPosition: 'center',
+                                    }}
+                                />
+                                <div className='services-capabilities'>
+                                    <div>
+                                        <h4>{services.acf.capability_1_title}</h4>
+                                        <img src={services.acf.capability_1_img.url} alt='capability 1' />
+                                        <p>{services.acf.capability_1_description}</p>
+                                    </div>
+                                    <div>
+                                        <h4>{services.acf.capability_2_title}</h4>
+                                        <img src={services.acf.capability_2_img.url} alt='capability 2' />
+                                        <p>{services.acf.capability_2_description}</p>
+                                    </div>
+                                </div>
                             </div>
                         );
                     }
