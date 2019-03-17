f870525dc8fca70d379dc4d90c83a68625535cf2
Jason Cheong <jasonmcheong@gmail.com>
2019-03-17 00:02:12 -0600

    added form validation to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-const Contact = () => {
-    return (
+const Contact = ({ errors, touched }) => (
+    <Form>
         <div>
-            <h1>Contact</h1>
+            {touched.name && errors.name && <p>{errors.name}</p>}
+            <Field type='text' name='name' placeholder='Name' />
         </div>
-    );
-};
+        <div>
+            {touched.email && errors.email && <p>{errors.email}</p>}
+            <Field type='email' name='email' placeholder='Email' />
+        </div>
+        <button>Submit</button>
+    </Form>
+);
 
-export default Contact;
+const FormikContact = withFormik({
+    mapPropsToValues() {
+        return {
+            name: '',
+            email: '',
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+            .min(2)
+            .required('You must provide a name'),
+        email: Yup.string()
+            .email('Please enter a valid email address')
+            .required('Please enter your email address'),
+    }),
+    handleSubmit(values) {
+        console.log(values);
+    },
+})(Contact);
+
+export default FormikContact;
