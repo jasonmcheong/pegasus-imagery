bb8f4932875baa39573984b5271d9f6d83c6d980
Jason Cheong <jasonmcheong@gmail.com>
2019-03-22 19:32:23 -0600

    added scroll to top functionality to full job description

--- src/components/CareerDetail.js
+++ src/components/CareerDetail.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ScrollToTop from '../helper/ScrollToTop';
 
 const CareerDetail = props => {
     console.log('match', props.match);
     console.log(props);
     return (
         <div>
+            <ScrollToTop />
             {props.careers.map(career => {
                 if (career.id == props.match.params.careerId) {
                     return (
                         <div className='full-job'>
-                            <li>
-                                <Link to='/careers'>Go Back</Link>
-                            </li>
+                            <ul>
+                                <li className='back-btn-li'>
+                                    <Link className='back-btn' to='/careers'>
+                                        Go Back
+                                    </Link>
+                                </li>
+                            </ul>
                             <h2>{career.title.rendered}</h2>
                             <div
                                 className='full-job-desc'
