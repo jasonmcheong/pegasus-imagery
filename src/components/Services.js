dd3a92834f6d2842a9810c9f9fdbca8f6a9cd0fc
Jason Cheong <jasonmcheong@gmail.com>
2019-04-06 22:58:03 -0600

    adjusting the services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -20,13 +20,13 @@ class Services extends React.Component {
                     if (services.title.rendered === 'Services') {
                         return (
                             <div>
-                                <div className='header'>
-                                    <div className='header-inner'>
+                                <div className="header">
+                                    <div className="header-inner">
                                         <h1>{services.acf.heading}</h1>
                                         <div dangerouslySetInnerHTML={{ __html: services.acf.heading_description }} />
                                     </div>
                                 </div>
-                                <div className='service-tabs'>
+                                <div className="service-tabs">
                                     <div
                                         onClick={evt =>
                                             this.handleClick(
@@ -41,9 +41,9 @@ class Services extends React.Component {
                                     >
                                         <img
                                             src={services.acf.service_1_icon.url}
-                                            alt='icon'
-                                            width='25px'
-                                            height='25px'
+                                            alt="icon"
+                                            width="25px"
+                                            height="25px"
                                         />
                                         <h2>{services.acf.service_1_title}</h2>
                                     </div>
@@ -61,9 +61,9 @@ class Services extends React.Component {
                                     >
                                         <img
                                             src={services.acf.service_2_icon.url}
-                                            alt='icon'
-                                            width='25px'
-                                            height='25px'
+                                            alt="icon"
+                                            width="25px"
+                                            height="25px"
                                         />
                                         <h2>{services.acf.service_2_title}</h2>
                                     </div>
@@ -81,14 +81,14 @@ class Services extends React.Component {
                                     >
                                         <img
                                             src={services.acf.service_3_icon.url}
-                                            alt='icon'
-                                            width='25px'
-                                            height='25px'
+                                            alt="icon"
+                                            width="25px"
+                                            height="25px"
                                         />
                                         <h2>{services.acf.service_3_title}</h2>
                                     </div>
                                 </div>
-                                <div id='service-display'>
+                                <div id="service-display">
                                     <div>
                                         <h3>{services.acf.service_1_title}</h3>
                                         <p>{services.acf.service_1_description}</p>
@@ -96,7 +96,7 @@ class Services extends React.Component {
                                     <div dangerouslySetInnerHTML={{ __html: services.acf.service_1_video_link }} />
                                 </div>
                                 <div
-                                    className='services-infographic'
+                                    className="services-infographic"
                                     style={{
                                         width: '100%',
                                         height: '578px',
@@ -106,15 +106,15 @@ class Services extends React.Component {
                                         backgroundPosition: 'center',
                                     }}
                                 />
-                                <div className='services-capabilities'>
+                                <div className="services-capabilities">
                                     <div>
                                         <h4>{services.acf.capability_1_title}</h4>
-                                        <img src={services.acf.capability_1_img.url} alt='capability 1' />
+                                        <img src={services.acf.capability_1_img.url} alt="capability 1" />
                                         <p>{services.acf.capability_1_description}</p>
                                     </div>
                                     <div>
                                         <h4>{services.acf.capability_2_title}</h4>
-                                        <img src={services.acf.capability_2_img.url} alt='capability 2' />
+                                        <img src={services.acf.capability_2_img.url} alt="capability 2" />
                                         <p>{services.acf.capability_2_description}</p>
                                     </div>
                                 </div>
